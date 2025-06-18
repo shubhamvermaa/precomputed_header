4bd5f881ddb71663384afa1cbddb2165fa1095d1
shubhamvermaa <vshubham0710@gmail.com>
2025-06-18 16:35:53 +0530

    precompute headers

---
+++ bits/stdc++.h
@@ -0,0 +1,2 @@
+#include <bits/stdc++.h>
+// g++ -std=c++20 -x c++-header -o bits/stdc++.h.gch header.h// ubuntu
---
+++ ext/pb_ds/assoc_container.hpp
@@ -0,0 +1,2 @@
+#include <ext/pb_ds/assoc_container.hpp>
+//g++ -c header.h -o bits/stdc++.h.gch
