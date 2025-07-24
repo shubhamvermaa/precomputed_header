6cedfccae5aca97f832dec86951c3a896ce5a116
Shubham Verma <vshubham0710@gmail.com>
2025-07-25 00:13:38 +0530

    Update stdc++.h

--- bits/stdc++.h
+++ bits/stdc++.h
@@ -1,2 +1,7 @@
 #include <bits/stdc++.h>
-// g++ -std=c++20 -x c++-header -o bits/stdc++.h.gch inputHeader.h// ubuntu
+// g++ -std=c++20 -x c++-header -o outputHeaderName.h.gch inputHeaderName.h      // ubuntu
+
+// Example: Run this in the Codeforces Directory
+// g++ -std=c++20 -x c++-header -o bits/stdc++.h.gch bits/stdc++.h
+// g++ -std=c++20 -x c++-header -o ext/pb_ds/assoc_container.hpp.gch ext/pb_ds/assoc_container.hpp
+// g++ -std=c++20 -x c++-header -o ext/pb_ds/tree_policy.hpp.gch ext/pb_ds/tree_policy.hpp
